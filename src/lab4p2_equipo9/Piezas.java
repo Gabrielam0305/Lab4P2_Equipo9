971d98148000e7c7f18635d5d0edb916b0a9d91e
Luxtter <115840511+Luxtter@users.noreply.github.com>
2023-02-10 14:22:29 -0600

    Pieza

---
+++ src/lab4p2_equipo9/Piezas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4p2_equipo9;
+
+/**
+ *
+ * @author luism
+ */
+public abstract class Piezas {
+    protected int x,y;
+    protected String color;
+    protected String Representacion;
+    
+    public abstract void movimiento();
+}
