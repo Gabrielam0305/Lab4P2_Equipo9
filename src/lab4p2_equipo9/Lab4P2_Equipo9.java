d8fa38accbdb9d6c1e9e969d3f761c371f372a53
Gabrielam0305 <gabrielamontes0305@gmail.com>
2023-02-10 14:06:26 -0600

    Prueba

--- src/lab4p2_equipo9/Lab4P2_Equipo9.java
+++ src/lab4p2_equipo9/Lab4P2_Equipo9.java
@@ -14,7 +14,7 @@ public class Lab4P2_Equipo9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("h");
     }
     
 }
